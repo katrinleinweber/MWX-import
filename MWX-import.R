e64fd5875b32f14550f70a18bd4196ef7f7900c7
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2015-05-28 14:30:15 +0200

    improve explanations, comments & object names

--- MWX-import.R
+++ MWX-import.R
@@ -1,53 +1,67 @@
-# Import function for .MWX files created by
+# Import function for .MWX files created by the
 # "Swift II Multi Wavelength Version 2.00" program (Biochrom Ltd)
-# ensure MWX export by activating "Export Spreadsheet" under "Run > Medhod > Run Options"
+# Important: Ensure MWX export by activating "Export Spreadsheet" under
+# "Run > Medhod > Run Options"
 
 library(reshape2) # needed for colsplit
 
-import_MWXs <- function(skip,
-                        # parameter defaults
-                        clean = TRUE,
-                        name_sep = FALSE,
-                        new_names = FALSE) {
+import_MWX_files <- function(skip_lines,
+                             # parameter defaults
+                             clean = TRUE,
+                             var_sep = FALSE,
+                             new_vars = FALSE
+                             ) {
 
-  # MWX files have to result from same method
-  MWX_files <- list.files(getwd(), pattern = "\\.MWX")
+  # Please note: MWX files should be present in R's working directory, and
+  # should have resulted from same method. Otherwise, different numbers of
+  # header lines, variables, etc. may cause this script to run into errors.
+  MWX_files <- list.files(getwd(),
+                          # setwd(dirname(file.choose())) # alternative selection
+                          # of working directory via file selection dialogue
+                          pattern = "\\.MWX"
+                          )
 
-  # concatenate data into data.frame
-  MWX_data <- do.call("rbind",
-                      lapply(X = MWX_files,
-                             FUN = read.table,
-                             stringsAsFactors = FALSE,
-                             header = TRUE,
-                             skip = skip,
-                             sep = "\t",
-                             dec = ",")
-                      )
+  # merge data tables from MWX file into R dataframe
+  MWX_df <- do.call("rbind",
+                    lapply(X = MWX_files,
+                           FUN = read.table,
+                           stringsAsFactors = FALSE,
+                           header = TRUE,
+                           skip = skip_lines,
+                           sep = "\t",
+                           dec = ","
+                           )
+                    )
 
-  # remove default observation & variable
+  # remove default observations & variables
   if (clean) {
-    MWX_data <- MWX_data[MWX_data$Sample != "Reference",]
-    MWX_data$X <- NULL
-    rownames(MWX_data) <- NULL
+    MWX_df <- MWX_df[MWX_df$Sample != "Reference",]
+    MWX_df$X <- NULL
+    rownames(MWX_df) <- NULL
   }
 
   # split sample names into variables
-  if (name_sep != FALSE) {
-    MWX_data <- cbind(MWX_data,
-                      colsplit(string = MWX_data$Sample,
-                               pattern = name_sep,
-                               names = new_names)
-                      )
+  if (var_sep != FALSE) {
+    MWX_df <- cbind(MWX_df,
+                    colsplit(string = MWX_df$Sample,
+                             pattern = var_sep,
+                             names = new_vars
+                             )
+                    )
   }
-  return(MWX_data)
+  return(MWX_df)
 }
 
 # import demo with example files
-# adjust parameters & observe MWX_data object in RStudio
-MWX_data <- import_MWXs(skip = 13,
-                        clean = FALSE,
-                        name_sep = "_",
-                        # Sample names like "150401aa_ax_manual_4" work-around
-                        # in Swift II for encoding variables
-                        new_names = c("Plate", "Achmi", "Pipette", "uL_MeOH")
-                        )
+# adjust parameters & observe MWX_df object in RStudio
+MWX_demo <- import_MWX_files(skip_lines = 13,
+                             clean = FALSE,
+                             var_sep = "_",
+                             # Use this as a work-around for encoding variables names
+                             # in Swift II, e.g. "150401aa_ax_manual_4" turns into:
+                             new_vars = c("plate_ID",
+                                          "cell_culture",
+                                          "treatment",
+                                          "mL_solvent"
+                                          )
+                             )
