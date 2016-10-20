5778e71a1503639114c940718d82fd9444ad3bb9
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-10-20 14:19:44 +0200

    CMD+I in RStudio to demonstrate GitHub’s blame UI

--- MWX-import.R
+++ MWX-import.R
@@ -10,8 +10,8 @@ import_MWX_files <- function(skip_lines,
                              clean = TRUE,
                              var_sep = FALSE,
                              new_vars = FALSE
-                             ) {
-
+) {
+  
   # Please note: MWX files should be present in R's working directory, and
   # should have resulted from same method. Otherwise, different numbers of
   # header lines, variables, etc. may cause this script to run into errors.
@@ -19,8 +19,8 @@ import_MWX_files <- function(skip_lines,
                           # setwd(dirname(file.choose())) # alternative selection
                           # of working directory via file selection dialogue
                           pattern = "\\.MWX"
-                          )
-
+  )
+  
   # merge data tables from MWX file into R dataframe
   MWX_df <- do.call("rbind",
                     lapply(X = MWX_files,
@@ -30,24 +30,24 @@ import_MWX_files <- function(skip_lines,
                            skip = skip_lines,
                            sep = "\t",
                            dec = ","
-                           )
                     )
-
+  )
+  
   # remove default observations & variables
   if (clean) {
     MWX_df <- MWX_df[MWX_df$Sample != "Reference",]
     MWX_df$X <- NULL
     rownames(MWX_df) <- NULL
   }
-
+  
   # split sample names into variables
   if (var_sep != FALSE) {
     MWX_df <- cbind(MWX_df,
                     colsplit(string = MWX_df$Sample,
                              pattern = var_sep,
                              names = new_vars
-                             )
                     )
+    )
   }
   return(MWX_df)
 }
@@ -63,5 +63,5 @@ MWX_demo <- import_MWX_files(skip_lines = 13,
                                           "cell_culture",
                                           "treatment",
                                           "mL_solvent"
-                                          )
                              )
+)
