c38c6fde0cd3ba0f6212e26987960e65a96123ac
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-03-09 21:09:47 +0100

    start docu with roxygen

--- R/read_mwx.R
+++ R/read_mwx.R
@@ -1,15 +1,24 @@
 
+#' import_MWX_files
+#'
+#' Imports and cleans .MWX files created by the "Swift II Multi Wavelength Version 2.00" program (Biochrom Ltd). To obtain those files activate "Export Spreadsheet" under "Run > Medhod > Run Options".
+#'
+#' Please note: the .mwx files must have resulted from the same Swift II method. Otherwise, different numbers of header lines, variables, etc. might derail this function.
+#'
+#' @param skip_lines integer: the number of lines of the data file to skip before beginning to read data.
+#' @param clean Determines whether the `X` column and rownames should be removed.
+#' @param var_sep a character vector used to split untidily concatenated `Sample` names into their own columns, named according to `new_vars`.
+#' @param new_vars a combination of strings to be used as variable names after splitting at `var_sep`.
+#'
+#' @export
+#'
 import_MWX_files <- function(skip_lines,
                              # parameter defaults
                              clean = TRUE,
                              var_sep = FALSE,
                              new_vars = FALSE
 ) {
-  
-  # Please note: MWX files should be present in R's working directory, and
-  # should have resulted from same method. Otherwise, different numbers of
-  # header lines, variables, etc. may cause this script to run into errors.
-  MWX_files <- list.files(getwd(),
+
                           # setwd(dirname(file.choose())) # alternative selection
                           # of working directory via file selection dialogue
                           pattern = "\\.MWX"
